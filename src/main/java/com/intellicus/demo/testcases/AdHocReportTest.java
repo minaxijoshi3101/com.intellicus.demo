55da6a410973174384307374b2eea2c5118b2380
minaxijoshi3101 <minaxijoshi3101@gmail.com>
2018-09-23 14:01:14 +0530

    Update AdHocReportTest.java
    added import org.testng.annotations

--- src/main/java/com/intellicus/demo/testcases/AdHocReportTest.java
+++ src/main/java/com/intellicus/demo/testcases/AdHocReportTest.java
@@ -4,7 +4,7 @@
 package com.intellicus.demo.testcases;
 
 import java.io.IOException;
-
+import org.testng.annotations;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
